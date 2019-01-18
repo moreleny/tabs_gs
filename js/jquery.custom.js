4bb1e30fb1b0bd15ca7c5e1c1c1d1884069e570b
moreleny <42089697+moreleny@users.noreply.github.com>
2019-01-18 17:00:35 +0300

    так чуть

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -1,9 +1,8 @@
 jQuery(document).ready(function(){
 	$('.fullname-window').mask('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', {translation: {'X': {pattern: /[А-Яа-я-" "]/, optional: false } } });
-//$('.phone-window').mask('+7 (000) 000 00 00', {placeholder: "+7 (___) ___ __ __"});
+	$('.phone-window').mask('+7 (000) 000 00 00', {placeholder: "+7 (___) ___ __ __"});
+	$('.main-form').validate();
 });
 
-jQuery(document).ready(function(){
- $('.main-form').validate();
-});
+
 
