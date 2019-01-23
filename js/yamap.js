e73d093404e1b24097a253011bbe6f83a0096d96
moreleny <42089697+moreleny@users.noreply.github.com>
2019-01-23 17:31:20 +0300

    добавил метки

--- js/yamap.js
+++ js/yamap.js
@@ -1,19 +1,43 @@
-var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.776062, 37.610997],
         zoom: 10
     }, {
-        //autoFitToViewport: 'always',
-        searchControlProvider: 'yandex#search'
-    });
-}
+            searchControlProvider: 'yandex#search'
+        }),
 
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myFirstPlacemark = new ymaps.Placemark([55.801131, 37.508167], {
+            hintContent: 'Песчаная улица, 13'
+            
+        }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '../img/marker.png',
+                // Размеры метки.
+                iconImageSize: [32, 44]
+            }),
+
+        mySecondPlacemark = new ymaps.Placemark([55.757556, 37.651592], {
+            hintContent: 'Подсосенский переулок, 11'
+            
+        }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '../img/marker.png',
+                // Размеры метки.
+                iconImageSize: [32, 44]
+            });
+
+    myMap.geoObjects
+        .add(myFirstPlacemark)
+        .add(mySecondPlacemark);
+});
